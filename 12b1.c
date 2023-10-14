83376ced9e2d7eca8b3cce50787ee4646938821a
Harshil solanki <146664906+26harshil@users.noreply.github.com>
2023-10-14 13:28:41 +0530

    Add files via upload

---
+++ 12b1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,j,a=0,sum=0;
+
+   printf("enter the value : ");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+   	
+   	for(j=1;j<=i;j++)
+   	{
+   		sum=sum+j;
+   	
+	   }
+	   
+}
+	   printf("%d",sum);
+   	
+
+ 
+  }
+
